58bd4717e9249d25c413a99643a65e18376d898c
kenshin0413 <kenshin1234569@gmail.com>
2025-10-12 15:16:26 +0900

    初期

---
+++ Memo-app/Memo-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memo-app
+//
+//  Created by miyamotokenshin on R 7/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memo-app/Memo-app/Memo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memo_appApp.swift
+//  Memo-app
+//
+//  Created by miyamotokenshin on R 7/10/12.
+//
+
+import SwiftUI
+
+@main
+struct Memo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
